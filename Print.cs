23a48054df4a0517c48199bf17d4fc6092920884
Martin <martin@happyjazz.eu>
2014-09-01 11:44:00 +0200

    Initial Commit

---
+++ Print.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FirstThread
+{
+    class Print
+    {
+        public void PrintMethod(object message, int numberOfTimes, string threadName, int sleepTimer = 0)
+        {
+            if (message.GetType() != typeof(string))
+            {
+                throw new ArgumentException("Message must be a string!", "message");
+            }
+
+            for (int i = 0; i < numberOfTimes; i++)
+            {
+                Thread.Sleep(sleepTimer);
+                int messageNumber = i;
+                Console.WriteLine(threadName + " " + message + " " + messageNumber);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FirstThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Print print = new Print();
+
+            Thread t1 = new Thread(() => print.PrintMethod("Hello World", 20, Thread.CurrentThread.Name, 500));
+            Thread t2 = new Thread(() => print.PrintMethod("Hello World", 20, Thread.CurrentThread.Name, 100));
+            Thread t3 = new Thread(() => print.PrintMethod("Hello World", 20, Thread.CurrentThread.Name));
+            Thread t4 = new Thread(() => print.PrintMethod("Hello World", 20, Thread.CurrentThread.Name));
+
+            t1.Name = "Thread1";
+            t2.Name = "Thread2";
+            t3.Name = "Thread3";
+            t4.Name = "Thread4";
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            t4.Start();
+        }
+    }
+}
